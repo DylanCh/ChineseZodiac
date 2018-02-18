07c281cbe3b085064de8a4dc4a285991983e709f
Dylan Chen <hchennyc@outlook.com>
2018-02-18 18:33:52 -0500

    Add Command line interface

--- src/main.java
+++ src/main.java
@@ -1,11 +1,19 @@
 /**
- * Created by aaa on 2/15/18.
+ * Created by Hanjun Chen on 2/15/18.
  */
 import com.*;
 
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args){
-
-        System.out.println(new Handler().get_ChineseZodiac(2018).name);
+        Scanner s = new Scanner(System.in);
+        System.out.println("What's birth year?");
+        try {
+            System.out.println(new Handler().get_ChineseZodiac(Integer.parseInt(s.next())).name);
+        }
+        catch (NumberFormatException e){
+            System.out.println("Your birth year's format is wrong");
+        }
     }
 }
